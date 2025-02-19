4947cbc281f85236741f04306cdc424548f3b3ee
isaiasleibo <isaias.leibovich@gmail.com>
2025-02-19 00:53:06 -0300

    .

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -4,13 +4,6 @@ module.exports = {
         name: "aprendo_mas_frontend",
         script: "serve -s build -l 3001", 
         watch: ["build"], 
-      },
-      {
-        name: "aprendo_mas_frontend_build",
-        script: "npm",
-        args: "run build",
-        watch: ["src", "public"], 
-        autorestart: false,
       }
     ]
   };
